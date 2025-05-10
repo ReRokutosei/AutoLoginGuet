fd72bf60a44d8bdaf63e6f5fca2c3cbad91092c8
ReRokutosei <xiao.yuel@qq.com>
2025-05-10 23:31:42 +0800

    fix:使用vbs隐藏终端黑框

--- Bat脚本/Rust-取消开机自启-注册表.bat
+++ Bat脚本/Rust-取消开机自启-注册表.bat
@@ -9,4 +9,15 @@ if %errorlevel% equ 0 (
     echo Notice: Auto-start registry entry not found.
 )
 
+:: 删除生成的 VBS 脚本文件
+set "current_dir=%~dp0"
+set "vbs_name=AutoLogin.vbs"
+set "vbs_path=%current_dir%%vbs_name%"
+if exist "%vbs_path%" (
+    del /q "%vbs_path%"
+    echo Info: VBS script file has been deleted.
+) else (
+    echo Info: VBS script file not found.
+)
+
 pause
